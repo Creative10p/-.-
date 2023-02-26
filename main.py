d7c8f4059075b1a76ca170373b5977512fd5a194
Creative10p <117911578+Creative10p@users.noreply.github.com>
2023-02-26 10:44:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,109 @@
+import pygame
+import os
+import sys
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+
+def load_image(name, colorkey=None):
+    fullname = name
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+def load_level(filename):
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mar.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
+    def update(self, *args):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 1
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 1
+        if keys[pygame.K_UP]:
+            self.rect.y -= 1
+        if keys[pygame.K_DOWN]:
+            self.rect.y += 1
+
+# основной персонаж
+player = None
+
+# группы спрайтов
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+name_of_map = input()
+try:
+    player, level_x, level_y = generate_level(load_level(name_of_map))
+except:
+    print("Ошибка!")
+    sys.exit()
+fps = 50  # количество кадров в секунду
+clock = pygame.time.Clock()
+running = True
+while running:  # главный игровой цикл
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    tiles_group.draw(screen)
+    player_group.draw(screen)
+    player_group.update(event)
+    pygame.display.flip()
+pygame.quit()
